e3ed075a4386ff42e57d3909bfc75a2d4f979d3e
KishorekumarBS <hksankaran@gmail.com>
2024-07-01 15:48:50 -0700

    Final commit

--- BudgetApp_JSproject/script.js
+++ BudgetApp_JSproject/script.js
@@ -12,44 +12,91 @@ formEl.addEventListener('submit', function(event){
     const amount = +inputAmountEl.value;
     console.log(description, amount)
 
-    const transactionItemHTML = `
-    <li class="transaction transaction--income">
+    
+
+    if (amount>0){
+        const currentIncome = +numberIncomeEl.textContent;
+        const updatedIncome = currentIncome + amount;
+        numberIncomeEl.textContent = updatedIncome;
+        const balancenum = +balanceNumberEl.textContent;
+        const updatedbalance = balancenum + amount;
+        balanceNumberEl.textContent = updatedbalance
+        if (updatedbalance>0){
+            balanceNumberEl.style.color = 'black'
+        }
+        const transactionItemHTML = `
+        <li class="transaction transaction--income">
           <span class="transaction__text">${description}</span>
           <span class="transaction__amount">+${amount}</span>
           <button class="transaction__btn">X</button>
-    </li>
+        </li>
     `;
-    transactionsEl.insertAdjacentHTML('beforeend',transactionItemHTML);
+        transactionsEl.insertAdjacentHTML('beforeend',transactionItemHTML);
+    }
+    else{
+        const currentExpense = +numberExpensesEl.textContent;
+        const updatedExpenses = currentExpense - (amount);
+        numberExpensesEl.textContent = updatedExpenses;
+        const balancenum = balanceNumberEl.textContent;
+        const updatedbalance = balancenum - (amount*-1);
+        balanceNumberEl.textContent = updatedbalance
+        if (updatedbalance<0){
+            balanceNumberEl.style.color = 'red'
+        }
+        const transactionItemHTML = `
+        <li class="transaction transaction--expense">
+            <span class="transaction__text">${description}</span>
+            <span class="transaction__amount">${amount}</span>
+            <button class="transaction__btn">X</button>
+        </li>
+    `;
+        transactionsEl.insertAdjacentHTML('beforeend',transactionItemHTML);
+    }
+
+    
+    inputDescriptionEl.value='';
+    inputAmountEl.value='';
+
+    inputDescriptionEl.blur();
+    inputAmountEl.blur();
     
 
 });
 
-function clickhandler(event){ 
-    const clickedEl = event.target.parentNode;
-    clickedEl.remove();
+const clickHandler = (event) => {
+    // remove transaction item visually
+    if (event.target.classList.contains('transaction__btn')) {
+        // remove transaction item visually
+        const clickedEl = event.target.parentNode;
+        clickedEl.remove();
 
-    const amountEl = clickedEl.querySelector('.transaction__amount')
-    const amount = +amountEl.textContent;
-    if (amount>0){
-        const currentIncome = numberIncomeEl.textContent;
-        const updatedIncome = currentIncome - amount;
-        numberIncomeEl.textContent = updatedIncome;
-    }
-    else{
-        const currentExpense = numberExpensesEl.textContent;
-        const updatedExpenses = currentExpense - (amount*-1);
-        numberExpensesEl.textContent = updatedExpenses;
-    }
+        // update income or expenses
+        const amountEl = clickedEl.querySelector('.transaction__amount');
+        const amount = +amountEl.textContent;
 
-    const income =+numberIncomeEl.textContent;
-    const expenses =+numberExpensesEl.textContent;
-    balanceNumberEl.textContent = income - expenses;
-    if (income - expenses< 0){
-        balanceNumberEl.style.color = 'red';  
-    }
-}
+        if (amount > 0) {
+            const currentIncome = +numberIncomeEl.textContent;
+            const updatedIncome = currentIncome - amount;
+            numberIncomeEl.textContent = updatedIncome;
+        } else {
+            const currentExpenses = +numberExpensesEl.textContent;
+            const updatedExpenses = currentExpenses - (amount * -1);
+            numberExpensesEl.textContent = updatedExpenses;
+        }
 
-transactionsEl.addEventListener('click',clickhandler)
+        // update balance
+        const income = +numberIncomeEl.textContent;
+        const expenses = +numberExpensesEl.textContent;
+        balanceNumberEl.textContent = income - expenses;
+
+        // make red if balance negative
+        if (income - expenses < 0) {
+            balanceNumberEl.style.color = 'red';
+        }
+    }
+  };
+  
+  transactionsEl.addEventListener('click', clickHandler);
 function calculateBalance (income,expenses){
     return income-expenses; 
 } 
